5022443407de9baf1715e3138e42b103f104cdd2
auto-robot <auto@example.com>
2025-08-09 17:56:21 -0700

    [AUTO] teardown all source file

--- sast/php/CWE_346.php
+++
@@ -1,8 +0,0 @@
-<?php
-  session_start();
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-      $quantity = $_POST['quantity'];
-      $recipient = $_POST['recipient'];
-      echo "Transfer $quantity to $recipient";
-  }
-?>
